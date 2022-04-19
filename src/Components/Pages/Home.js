57b14f2c5a63ec195e1defcc65a888d1f362f5b5
Tech-X <30934772+1Tech-X@users.noreply.github.com>
2022-04-19 19:28:40 +0530

    Update Home.js

--- src/Components/Pages/Home.js
+++ src/Components/Pages/Home.js
@@ -2,7 +2,6 @@ import React from 'react'
 import Hero from '../Hero/Hero'
 import Footer from '../Footer/Footer'
 import { Services } from '../Services/Services'
-import Review from '../Reviews/Review'
 import Offer from '../Offer/Offer'
 export const Home = () => {
   return (
@@ -10,7 +9,6 @@ export const Home = () => {
         <Hero />
         <Services />
         <Offer />
-        <Review />
         <Footer/>
     </div>
   )
